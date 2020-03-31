b035d9b52ea52a0699b9cc767e24851a016b3a78
Marsjaii_glb <majichao0530_glb@outlook.com>
2020-03-31 18:11:18 +0800

    完成登录界面的维持登录和自动登录

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import ReactDom from "react-dom";
 
 import App from "./App.js";
 
+import memoryUtils from "./utils/memoryUtils";
+import storageUtils from "./utils/storageUtils";
+// 读取local中的user并保存到内存中
+const user = storageUtils.getUser();
+memoryUtils.user = user;
+
 // 将App组件标签渲染到index页面的div中
 ReactDom.render(<App />, document.getElementById("root"));
--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -2,8 +2,17 @@
 
 import React, { Component } from "react";
 
+import memoryUtils from "../../utils/memoryUtils";
+import { Redirect } from "react-router-dom";
+
 export default class Admin extends Component {
   render() {
-    return <div>Admin</div>;
+    const user = memoryUtils.user;
+    // 内存中没有user => 没有登录
+    if (!user || !user._id) {
+      // 自动跳转到登录界面
+      return <Redirect to="/login" />;
+    }
+    return <div>Hello {user.username}</div>;
   }
 }
